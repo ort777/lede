6c05aa3a3b6900a945951f621ac948fdfdb44563
ort777 <82152837+ort777@users.noreply.github.com>
2021-04-09 11:59:07 +0000

    Update ruijie.mk

--- target/linux/ar71xx/image/ruijie.mk
+++ target/linux/ar71xx/image/ruijie.mk
@@ -4,5 +4,6 @@ define Device/ruijie_rg-est310
   DEVICE_MODEL := RG-EST310
   DEVICE_PACKAGES := kmod-usb2 kmod-ath10k-ct ath10k-firmware-qca9888-ct \
 	-uboot-envtools -swconfig
+  IMAGE_SIZE := 7744k
 endef
 TARGET_DEVICES += ruijie_rg-est310
